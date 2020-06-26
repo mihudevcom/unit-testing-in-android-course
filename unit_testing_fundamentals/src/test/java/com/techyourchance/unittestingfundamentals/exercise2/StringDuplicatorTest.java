dcb194d39839395e145e770d4ef23c31c70f609f
Mihai Boisteanu <mihai.boisteanu@gmail.com>
2020-06-26 22:38:25 +0300

    Finished exercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator stringDuplicator;
+
+    @Before
+    public void setUp() throws Exception {
+        stringDuplicator = new StringDuplicator();
+    }
+
+    @Test
+    public void concatenate_emptyString_returnEmptyString() {
+        String text = stringDuplicator.duplicate("");
+        assertThat(text, is(""));
+    }
+
+    @Test
+    public void concatenate_singleCharacter_returnRepeatedCharacter() {
+        String text = stringDuplicator.duplicate("a");
+        assertThat(text, is("aa"));
+    }
+
+    @Test
+    public void concatenate_text_returnRepeatedText() {
+        String text = stringDuplicator.duplicate("Mihai");
+        assertThat(text, is("MihaiMihai"));
+    }
 }
